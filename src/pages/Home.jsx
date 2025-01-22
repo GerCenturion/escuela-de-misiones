88e85d2c8a3776f38f752087b1ccdaa3f4c46017
Gerson <gercentu777@gmail.com>
2025-01-22 01:03:44 -0300

    Modalidad de estudio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,37 @@ import React from "react";
 const About = () => {
   return (
     <section>
-      <h1>Sobre Nosotros</h1>
+      <h2>Modalidad de Estudio</h2>
       <p>
-        La Escuela de Misiones tiene como objetivo formar y equipar a personas
-        para llevar el mensaje de esperanza a todos los rincones del mundo.
+        Conforme a los tiempos que vivimos, hemos adoptado la “modalidad
+        virtual” para dictar las materias de nuestro Seminario Bíblico. Se dicta
+        a través de videos que se envían por medio de la aplicación Telegram
+        semanalmente. Allí mismo el profesor de la materia eleva las tareas
+        semanales y está a disposición para evacuar cualquier duda o consulta.
       </p>
+      <p>
+        Las materias se dictan de marzo a noviembre de cada año. Cada materia
+        inicia el día 1º del mes y finaliza el último día del mismo. Los
+        primeros videos y tareas se entregan el día 1º del mes, y se envían
+        sucesivamente cada siete (7) días: el 8, el 15 y el 22.
+      </p>
+      <p>
+        Las tareas deben ser entregadas vía Telegram de manera privada al
+        profesor correspondiente antes del plazo de la siguiente entrega.
+      </p>
+      <h3>Inscripción</h3>
+      <p>
+        Al iniciar el Seminario, se envía por privado la Solicitud de
+        Inscripción Virtual, que debe ser completada. Una vez enviada, el
+        estudiante queda registrado como alumno regular.
+      </p>
+      <p>
+        No se cobra inscripción. El costo por materia es de $3.000 individual o
+        $5.000 por matrimonio, pagaderos por depósito, transferencia bancaria o
+        MercadoPago. El pago se realiza por materia, y no es necesario abonar en
+        caso de no tomar materias en un mes específico.
+      </p>
+      <p>Estamos a su disposición para cualquier consulta.</p>
     </section>
   );
 };
