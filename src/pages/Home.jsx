2b1ddfdb5afcd565017d4d700b41f274048753b7
Gerson <gercentu777@gmail.com>
2025-02-22 00:13:52 -0300

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../HomePage.css";
 
 const HomePage = () => {
   return (
-    <div className="container mt-5">
+    <div className="about-container">
+      {/* Breve Historial */}
+      <section className="mt-5">
+        <h2 className="text-center">HISTORIA</h2>
+        <p>
+          Nuestro Seminario Bíblico dio comienzos en el mes de marzo del año
+          1993 en la ciudad de San Nicolás, Provincia de Buenos Aires, sede
+          central de nuestra Institución, extendiéndose rápidamente a diferentes
+          ciudades del interior como Corrientes, Rosario (Sta. Fe), Pirané
+          (Formosa), Aguas Calientes (Jujuy), Güemes (Salta), Salto Argentino
+          (Bs. As.), Salta (Capital), Baradero (Bs. As.) y Arroyo Seco (Sta. Fe)
+          y en la localidad de La Unión en el Dpto. Rivadavia Banda Sur en el
+          chaco salteño.
+        </p>
+      </section>
+
       {/* Carrusel de Imágenes */}
       <div
         id="homeCarousel"
@@ -47,13 +63,7 @@ const HomePage = () => {
               alt="Slide 5"
             />
           </div>
-          <div className="carousel-item">
-            <img
-              src="/fotos/6.jpeg"
-              className="d-block w-100"
-              alt="Slide 6"
-            />
-          </div>
+
           <div className="carousel-item">
             <img
               src="/fotos/7.jpeg"
@@ -129,61 +139,72 @@ const HomePage = () => {
           <span className="visually-hidden">Siguiente</span>
         </button>
       </div>
-
-      {/* Contenido Informativo */}
+      {/* Objetivos */}
       <section className="mt-5">
-        <h2 className="text-center">PRESENTE</h2>
+        <h2 className="text-center">OBJETIVOS</h2>
         <p className="text-justify">
-          Le saludamos en el nombre del Señor Jesús y le invitamos a{" "}
-          <strong>
-            Capacitarse para un mejor y más eficiente servicio al Señor
-          </strong>
-          .
+          <em>
+            Nuestro objetivo es la preparación integral del alumno en lo
+            espiritual y académico para un mejor desenvolvimiento social.
+          </em>
         </p>
-        <p className="text-justify">
+        <p>
+          En cuanto a su espiritualidad el SEMINARIO BÍBLICO{" "}
+          <strong>“ESCUELA DE MISIONES”</strong>, a través del estudio
+          sistemático de la Biblia prepara al alumno para un mejor conocimiento
+          y relación con el Creador.
+        </p>
+        <p>
+          Referente a lo académico gracias a la cantidad de horas cátedras
+          implementadas llevamos al alumno a un conocimiento exhaustivo y
+          sistemático de la Biblia y su verdadera importancia en el desarrollo
+          personal, familiar y social.
+        </p>
+        <p>
+          Todo ello con el fin de lograr que el alumno sea un ciudadano
+          integrado a la sociedad en la cual se desenvuelve y a la vez sea de
+          aporte positivo y significativo en la misma al Servicio del Señor.
+        </p>
+        <p>
+          En pos de estos objetivos quienes dictan las materias correspondientes
+          son Siervos con idoneidad, capacitados en pleno ejercicio de sus
+          funciones ministeriales.
+        </p>
+        <p>
           Nuestro SEMINARIO BÍBLICO <strong>“ESCUELA DE MISIONES”</strong> le
           ofrece prepararse bíblicamente con una orientación Misionera y de
           expansión del Evangelio con la visión desde “casa”, “…hasta los
           confines de la tierra”.
         </p>
-        <p className="text-justify">
-          <strong>Conforme al Plan de Estudios presente en este folleto</strong>
-          , el curso es de un período de (4 cuatro) años en el que materias por
-          año, desde Marzo a Noviembre, haciendo un total de (36) treinta y seis
-          materias.
+        <p>
+          Conforme al Plan de Estudios el curso es de un período de 4 (cuatro)
+          cuatrimestres o 9 (nueve) materias por año, desde Marzo a Noviembre,
+          haciendo un total de 36 (treinta y seis) materias y seis materias más
+          de titulación (tesis y materias).
         </p>
-        <p className="text-justify">
-          Cada materia se dicta en una semana por mes de Lunes a Viernes, (3
-          tres) horas diarias, haciendo (5 cinco) horas mensuales, completando
-          el total de (14) catorce horas (quincenas cuatrimestrales) por año.
-        </p>
-        <p className="text-justify">
-          Al completar todo el Plan de Estudios (4 años) se entrega el{" "}
+        <p>
+          Al completar todo el Plan de Estudios (4 años) se entrega al alumno el{" "}
           <strong>Certificado de Estudios Bíblicos y Teológicos.</strong>
         </p>
-
-        <h2 className="text-center mt-5">OBJETIVOS</h2>
-        <p className="text-justify">
-          Nuestro objetivo es la preparación integral del alumno en lo
-          espiritual y académico para un mejor desenvolvimiento social.
-        </p>
-        <p className="text-justify">
-          En cuanto a su espiritualidad el SEMINARIO BÍBLICO{" "}
-          <strong>“ESCUELA DE MISIONES”</strong>, a través del estudio
-          sistemático de la palabra de Dios en manera profunda y relevante, lo
-          capacita para un mejor conocimiento y relación con el Creador.
-        </p>
-        <p className="text-justify">
-          Al inscribirse, sin costo de inscripción, el alumno recibe la
-          <strong> “SOLICITUD DE INSCRIPCIÓN”</strong> debidamente firmada y
-          cumplimentada ante la firma de Autorización de su Pastor y el
-          <strong> “RECLAMO INTERNO”</strong> donde encontrará el reglamento
-          académico administrativo.
-        </p>
+      </section>
+      <section>
         <p className="text-center mt-4">
           <strong>“ESTE ES EL TIEMPO DE DIOS”</strong>
         </p>
-
+        {/* Invitación a Inscribirse */}
+        <section className="mt-5 text-center">
+          <h2>¡Inscríbete Ahora!</h2>
+          <p>
+            ¿Quieres formar parte de nuestra Escuela de Misiones? Da el primer
+            paso en tu camino de preparación espiritual y académica.
+          </p>
+          <Link
+            to="/registration"
+            className="btn btn-primary"
+          >
+            Inscribirme
+          </Link>
+        </section>
         <div className="text-end">
           <p>
             <strong>MIGUEL ÁNGEL ZAMPERDI</strong>
